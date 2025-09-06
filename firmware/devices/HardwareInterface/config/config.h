7a205c2f8661337806cb48ae9252b6ae53743569
Yannik Süssmuth <yannik.suessmuth@web.de>
2025-09-06 20:43:22 +0000

    add config files for Hardware Interface

---
+++ firmware/devices/HardwareInterface/config/config.h
@@ -0,0 +1,12 @@
+/**
+ * @file config.h
+ *
+ * @brief CAuDri - Configuration file for the Hardware Interface firmware
+ * 
+ * This file is used to set up all config parameters for hardware drivers and micro-ROS nodes.
+ * It defines additional pin mappings and configuration options specific to the Hardware Interface.
+ */
+
+#pragma once
+
+#include "main.h"
