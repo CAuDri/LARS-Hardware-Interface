27928bbdc4817c0db701ea9f50987e3a821428b2
Yannik Süssmuth <yannik.suessmuth@web.de>
2025-10-02 17:26:09 +0000

    add trace recorder init and jump to main application entry

---
+++ firmware/devices/HardwareInterface/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * 
+ * @brief CAuDri - Main entry point for the application code
+ */
+
+#include "main.h"
+
+extern "C" void mainTask();
+
+/**
+ * @brief Main entry point called from the RTOS task in main.c
+ */
+void mainTask()
+{}
